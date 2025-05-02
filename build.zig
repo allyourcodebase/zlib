01b86877f1e5fa53a3bdaeb38e4ee10ce5e3a968
jd <jean.daniel.michaud@gmail.com>
2025-05-02 07:03:02 +0200

    Add dynamic lib + single threaded option

--- build.zig
+++ build.zig
@@ -1,16 +1,24 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const upstream = b.dependency("zlib", .{});
-    const lib = b.addStaticLibrary(.{
-        .name = "z",
-        .target = b.standardTargetOptions(.{}),
-        .optimize = b.standardOptimizeOption(.{}),
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const single_threaded = b.option(bool, "single-threaded", "Build artifacts that run in single threaded mode");
+
+    const upstream = b.dependency("zlib", .{
+        .target = target,
+        .optimize = optimize,
     });
-    lib.linkLibC();
-    lib.addCSourceFiles(.{
-        .root = upstream.path(""),
-        .files = &.{
+
+    const lib_mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+        .single_threaded = single_threaded,
+    });
+    lib_mod.addCSourceFiles(.{
+        .root = upstream.path("."),
+        .files = &[_][]const u8{
             "adler32.c",
             "crc32.c",
             "deflate.c",
@@ -34,6 +42,12 @@ pub fn build(b: *std.Build) void {
             "-DZ_HAVE_UNISTD_H",
         },
     });
+
+    const lib = b.addLibrary(.{
+        .linkage = .static,
+        .name = "z",
+        .root_module = lib_mod,
+    });
     lib.installHeadersDirectory(upstream.path(""), "", .{
         .include_extensions = &.{
             "zconf.h",
@@ -41,4 +55,24 @@ pub fn build(b: *std.Build) void {
         },
     });
     b.installArtifact(lib);
+
+    const dynamic_lib = b.addLibrary(.{
+        .linkage = .dynamic,
+        .name = "z",
+        .root_module = lib_mod,
+    });
+    // Install with no version extension
+    b.installArtifact(dynamic_lib);
+    // Install with version extension
+    const output_name = b.fmt("libz{s}.{s}", .{
+        dynamic_lib.root_module.resolved_target.?.result.dynamicLibSuffix(),
+        "1.3.1",
+    });
+    const install_step = b.addInstallArtifact(dynamic_lib, .{
+        .dest_dir = .{
+            .override = .lib,
+        },
+        .dest_sub_path = output_name,
+    });
+    b.getInstallStep().dependOn(&install_step.step);
 }
